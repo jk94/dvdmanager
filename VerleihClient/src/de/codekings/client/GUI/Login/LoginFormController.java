eafce11c947686f3c5f54154399e07751814cec0
Jan <jankoschke@googlemail.com>
2014-10-11 12:41:24 +0200

    adsf#

--- VerleihClient/src/de/codekings/client/GUI/Login/LoginFormController.java
+++ VerleihClient/src/de/codekings/client/GUI/Login/LoginFormController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.codekings.client.gui.login;
+package de.codekings.client.GUI.Login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- VerleihClient/src/de/codekings/client/GUI/Login/login.java
+++ VerleihClient/src/de/codekings/client/GUI/Login/login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.codekings.client.gui.login;
+package de.codekings.client.GUI.Login;
 
 import java.io.IOException;
 import javafx.application.Application;
