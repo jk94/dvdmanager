518daa0c8e56fbaf20bf1947f62148a4ca19f9dd
sibest <strobesim@gmail.com>
2014-10-27 11:41:21 +0100

    controller

--- VerleihClient/src/de/codekings/client/GUI/Mitarbeiter/Create_userController.java
+++ VerleihClient/src/de/codekings/client/GUI/Mitarbeiter/Create_userController.java
@@ -6,15 +6,21 @@
 package de.codekings.client.GUI.Mitarbeiter;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import se.mbaeumer.fxmessagebox.MessageBox;
+import se.mbaeumer.fxmessagebox.MessageBoxType;
 
 /**
  * FXML Controller class
@@ -31,15 +37,9 @@ public class Create_userController implements Initializable {
     @FXML
     private PasswordField user_passwort_check;
     @FXML
-    private Button user_btnspeichern;
-    @FXML
     private DatePicker user_datum;
     @FXML
     private ListView<?> user_list;
-    @FXML
-    private Button user_btnspeichern1;
-    @FXML
-    private Button user_btnspeichern11;
     @FXML
     private TextField user_input_email;
     @FXML
@@ -54,6 +54,18 @@ public class Create_userController implements Initializable {
     private TextField user_input_ort;
     @FXML
     private TextField user_input_plz;
+    @FXML
+    private Button user_btn_create;
+    @FXML
+    private CheckBox user_check_kunde;
+    @FXML
+    private CheckBox user_check_mitarbeiter;
+    @FXML
+    private CheckBox user_check_admin;
+    @FXML
+    private Button user_btn_verwerfen;
+    @FXML
+    private Button user_btnspeichern;
 
     /**
      * Initializes the controller class.
@@ -61,51 +73,73 @@ public class Create_userController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        user_btn_create.setOnMouseClicked((MouseEvent event) -> {
+            check_Input();
+            // schreibe eingaben in db
+        });
+        
+        user_btnspeichern.setOnMouseClicked((MouseEvent event) -> {
+            check_Input();
+            // ändere db nach eingabe
+        });
+        user_btn_verwerfen.setOnMouseClicked((MouseEvent event) -> {
+            //lade Seite neu, ohne eingaben zu behalten
+        });
+        
+    }
+    
+    public void check_Input () {
         String vorname = user_input_vorname.getText();
         if (!vorname.matches("[a-zA-Z]")){ 
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Vorname darf nur aus Buchstaben bestehen.");
+            MessageBox error = new MessageBox("Vorname darf nur aus Buchstaben bestehen.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
         String name = user_input_nachname.getText();
          if (!name.matches("[a-zA-Z]")){ 
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Name darf nur aus Buchstaben bestehen.");
+            MessageBox error = new MessageBox("Name darf nur aus Buchstaben bestehen.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
         String email = user_input_email.getText();
-         if (!name.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+         if (!email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
-             //TODO setHinweisVisible(true);
-            //     setHinweis("Email nicht validiert."); 
+            MessageBox error = new MessageBox("Email entspricht nicht vorgegebenem Format.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
          }
         String passwort = user_passwort.getText();
-        if (!name.matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})")){
-             //TODO setHinweisVisible(true);
-            //     setHinweis("Passwort Kriterien nicht erfüllt."); 
+        if (!passwort.matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})")){
+            MessageBox error = new MessageBox("Passwortkriterien nicht erfüllt.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
          }
         String passwort_check = user_passwort_check.getText();
-        if (!passwort.equals(passwort_check))
+        if (!passwort_check.equals(passwort_check))
         {
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Passwörter stimmen nicht überein!");
+            MessageBox error = new MessageBox("Passwörter stimmen nicht überein!",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
         
-        //  TODO   Welcher Datentyp?? geburtstag = user_datum.getValue();
+        LocalDate geburtstag = user_datum.getValue();
+        Date d_geb = new Date(geburtstag.toEpochDay());
+        
+        System.out.println(d_geb.toLocaleString());
         
         String plz = user_input_plz.getText();
         if (!vorname.matches("[0-9]")){ 
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Postleitzahl darf nur aus Zahlen bestehen.");
+            MessageBox error = new MessageBox("Postleitzahl darf nur aus Zahlen bestehen.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
         String ort = user_input_ort.getText();
         if (!vorname.matches("[a-zA-Z]")){ 
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Ort darf nur aus Buchstaben bestehen.");
+            MessageBox error = new MessageBox("Ort darf nur aus Buchstaben bestehen.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
         String str = user_input_adresse.getText();
         if (!vorname.matches("(?=.*\\d)(?=.*[a-zA-Z])")){ 
-            //TODO setHinweisVisible(true);
-            //     setHinweis("Adresse muss Hausnummer enthalten.");
+            MessageBox error = new MessageBox("Adresse muss Hausnummer enthalten.",MessageBoxType.OK_ONLY);
+            error.showAndWait();
         }
+        
+        //  TODO Checkbox ansprechen      if { user_check_kunde.isSelected(MouseEvent)}
     }    
+    
     
 }
