818f11a71977aa35447099b05792b1f7d98e6ca4
Jan <jankoschke@googlemail.com>
2014-10-29 11:25:43 +0100

    control fix

--- VerleihServer/src/de/codekings/server/controls/Control.java
+++ VerleihServer/src/de/codekings/server/controls/Control.java
@@ -28,6 +28,8 @@ public class Control {
     private CoverManager coverManager;
 
     public Control() {
+        theControl = this;
+
         //Initialisiere Log für alle!
         log = Logger.getLogger(Control.class.getSimpleName());
         loadConfig();
@@ -43,9 +45,6 @@ public class Control {
         
         //Initialisiere unverschlüsselten Server und startet ihn.
         runServer();
-
-        theControl = this;
-
     }
 
     public static Control getInstance() {
